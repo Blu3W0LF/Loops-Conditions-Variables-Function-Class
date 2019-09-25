31ad13ce45555ed422572654c25324b09cee2bc2
BlueWolf <sengoz.yavuz@gmail.com>
2019-09-25 17:09:25 +0300

    Loops, Conditions, Variables, Function, Class Windoes Application developed by Yavuz SENGOZ

---
+++ StajUygulamasiProgrami/StajUygulamasi/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace StajUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxSoyad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Baslık = new System.Windows.Forms.Label();
+            this.textBoxAd = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.radioButtonKadin = new System.Windows.Forms.RadioButton();
+            this.radioButtonErkek = new System.Windows.Forms.RadioButton();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.textBoxYas = new System.Windows.Forms.NumericUpDown();
+            this.buttonGoster = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxYas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Trebuchet MS", 11.25F);
+            this.label13.Location = new System.Drawing.Point(25, 201);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(43, 20);
+            this.label13.TabIndex = 235;
+            this.label13.Text = "Yaş :";
+            // 
+            // textBoxSoyad
+            // 
+            this.textBoxSoyad.AcceptsTab = true;
+            this.textBoxSoyad.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxSoyad.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSoyad.Location = new System.Drawing.Point(192, 163);
+            this.textBoxSoyad.Name = "textBoxSoyad";
+            this.textBoxSoyad.Size = new System.Drawing.Size(245, 25);
+            this.textBoxSoyad.TabIndex = 223;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(24, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 20);
+            this.label6.TabIndex = 233;
+            this.label6.Text = "Soyad :";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::StajUygulamasi.Properties.Resources.icons8_add_user_group_man_man_64;
+            this.pictureBox.Location = new System.Drawing.Point(230, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(64, 63);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 231;
+            this.pictureBox.TabStop = false;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(193, 233);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(245, 25);
+            this.dateTimePicker.TabIndex = 219;
+            // 
+            // Baslık
+            // 
+            this.Baslık.AutoSize = true;
+            this.Baslık.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Baslık.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Baslık.Location = new System.Drawing.Point(301, 41);
+            this.Baslık.Name = "Baslık";
+            this.Baslık.Size = new System.Drawing.Size(158, 33);
+            this.Baslık.TabIndex = 229;
+            this.Baslık.Text = " Yeni Kişi Ekle";
+            // 
+            // textBoxAd
+            // 
+            this.textBoxAd.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxAd.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAd.Location = new System.Drawing.Point(192, 131);
+            this.textBoxAd.Name = "textBoxAd";
+            this.textBoxAd.Size = new System.Drawing.Size(245, 25);
+            this.textBoxAd.TabIndex = 218;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(24, 131);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 20);
+            this.label11.TabIndex = 226;
+            this.label11.Text = "Adı  :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(25, 234);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 20);
+            this.label10.TabIndex = 225;
+            this.label10.Text = "Kayıt Tarihi :";
+            // 
+            // radioButtonKadin
+            // 
+            this.radioButtonKadin.AutoSize = true;
+            this.radioButtonKadin.Checked = true;
+            this.radioButtonKadin.Location = new System.Drawing.Point(209, 280);
+            this.radioButtonKadin.Name = "radioButtonKadin";
+            this.radioButtonKadin.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonKadin.TabIndex = 238;
+            this.radioButtonKadin.TabStop = true;
+            this.radioButtonKadin.Text = "Kadın";
+            this.radioButtonKadin.UseVisualStyleBackColor = true;
+            this.radioButtonKadin.CheckedChanged += new System.EventHandler(this.radioButtonKadin_CheckedChanged);
+            // 
+            // radioButtonErkek
+            // 
+            this.radioButtonErkek.AutoSize = true;
+            this.radioButtonErkek.Location = new System.Drawing.Point(325, 280);
+            this.radioButtonErkek.Name = "radioButtonErkek";
+            this.radioButtonErkek.Size = new System.Drawing.Size(53, 17);
+            this.radioButtonErkek.TabIndex = 239;
+            this.radioButtonErkek.Text = "Erkek";
+            this.radioButtonErkek.UseVisualStyleBackColor = true;
+            this.radioButtonErkek.CheckedChanged += new System.EventHandler(this.radioButtonErkek_CheckedChanged);
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(463, 129);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(118, 45);
+            this.buttonKaydet.TabIndex = 240;
+            this.buttonKaydet.Text = "KAYDET";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // textBoxYas
+            // 
+            this.textBoxYas.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxYas.Location = new System.Drawing.Point(192, 199);
+            this.textBoxYas.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.textBoxYas.Name = "textBoxYas";
+            this.textBoxYas.Size = new System.Drawing.Size(245, 25);
+            this.textBoxYas.TabIndex = 242;
+            this.textBoxYas.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // buttonGoster
+            // 
+            this.buttonGoster.Location = new System.Drawing.Point(463, 194);
+            this.buttonGoster.Name = "buttonGoster";
+            this.buttonGoster.Size = new System.Drawing.Size(118, 64);
+            this.buttonGoster.TabIndex = 243;
+            this.buttonGoster.Text = "Kayıt Edilen\r\nKişiyi Göster";
+            this.buttonGoster.UseVisualStyleBackColor = true;
+            this.buttonGoster.Click += new System.EventHandler(this.buttonGoster_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(709, 319);
+            this.Controls.Add(this.buttonGoster);
+            this.Controls.Add(this.textBoxYas);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.radioButtonErkek);
+            this.Controls.Add(this.radioButtonKadin);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBoxSoyad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.Baslık);
+            this.Controls.Add(this.textBoxAd);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Name = "Form1";
+            this.Text = "Sjaj Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxYas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label ad;
+        private System.Windows.Forms.Label soyad;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxSoyad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label Baslık;
+        private System.Windows.Forms.TextBox textBoxAd;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RadioButton radioButtonKadin;
+        private System.Windows.Forms.RadioButton radioButtonErkek;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.NumericUpDown textBoxYas;
+        private System.Windows.Forms.Button buttonGoster;
+    }
+}
+
---
+++ StajUygulamasiProgrami/StajUygulamasi/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StajUygulamasi
+{
+    public partial class Form1 : Form
+    {
+
+        // Değişken tipleri (Global)//
+        private bool kadinerkekkontrol;
+        string isim;
+        string soyisim;
+        int yas;
+        DateTime kayittarihi;
+        int[] erkek = { 5, 6, 7 };
+        string[] kadin = { "ayakkabı", "çanta", "elbise" };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Kayıt etme buttonu
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            // Girilen değerleri değişkenlere alıyoruz
+            isim = textBoxAd.Text;
+            soyisim = textBoxSoyad.Text;
+            yas =  Convert.ToInt32(textBoxYas.Value);
+            kayittarihi = dateTimePicker.Value;
+
+
+
+            // kontrollerden almış olduğumuz değerlerde false ise kadın dizinleriniz yazdırdık
+            if (!kadinerkekkontrol) 
+            {
+                for (int i = 0; i < kadin.Length; i++)
+                {
+                    MessageBox.Show(i+1+ ") ---> " + kadin[i], "kadın dize elemanları");
+                }
+                
+            }
+            else// kontrollerden almış olduğumuz değerlerde true ise erkek dizinleriniz yazdırdık
+            {
+                int say = 0;
+                foreach (int item in erkek)
+                {
+                    say++;
+                    MessageBox.Show(say + ") ---> " + item.ToString(), "erkek dize elemanları");
+                }
+                
+            }
+
+            KisiEkle();
+        }
+
+        // radio buttonlarından değişiklik yapıldıysa kadın için 'false', erkek için 'true' olarak ayarladık
+        private void radioButtonKadin_CheckedChanged(object sender, EventArgs e)
+        {
+            kadinerkekkontrol = false;
+        }
+
+        private void radioButtonErkek_CheckedChanged(object sender, EventArgs e)
+        {
+            kadinerkekkontrol = true;
+        }
+
+
+
+        // Kişisel Bilgiler için sınıf oluşturduk (nesne tabanlı programlama)
+        public class KisiBilgisi
+        {
+            public string ad { get; set; }
+            public string soyad { get; set; }
+            public int yas { get; set; }
+            public DateTime kayittarihi { get; set; }
+            public bool cinsiyeti { get; set; }
+
+        }
+
+        private KisiBilgisi KisiEkle() // Geriye Sınıf tipinde dönüş yapan bir fonksiyon oluşturduk.
+        {
+            // Değişkenlerdeki bilgileri oluşturmuş olduğumuzsınıftan boş bir eleman ürettik ve içerisine ekeldik.
+
+            KisiBilgisi kisiBilgisi = new KisiBilgisi();
+            kisiBilgisi.ad = isim;
+            kisiBilgisi.soyad = soyisim;
+            kisiBilgisi.yas = yas;
+            kisiBilgisi.kayittarihi = kayittarihi;
+            
+            // Radio button değişikliğinde kadın seçiliyse sınıf elemanımıza kadını false olarak ekledik.
+            if (!kadinerkekkontrol)
+            {
+                kisiBilgisi.cinsiyeti = false;
+            }
+            else // Radio button değişikliğinde kadın seçiliyse değil se sınıf elemanımıza kadını true olarak ekledik.
+            {
+                kisiBilgisi.cinsiyeti = true;
+            }
+
+            //Sınıf Elemanızmıza eklediğimiz Bilgileri döndürdük.
+            return kisiBilgisi;
+
+
+        }
+
+        private void buttonGoster_Click(object sender, EventArgs e)
+        {
+
+            //fonksiyondan gelene Bilgileri yazdırabilmek için sınıfımızdan yeni bir eleman oluşturduk ve fonksiyona eşitledik
+            KisiBilgisi kisiselBilgi = KisiEkle();
+
+            // ve istediğimiz bilgileri yazdırdık.
+            MessageBox.Show("Adı = "+ kisiselBilgi.ad+"\n" +
+                            "SoyAdı = " + kisiselBilgi.soyad + "\n" +
+                            "Kişi Yaşı = " + kisiselBilgi.yas + "\n" +
+                            "Kayıt Tarihi = " + kisiselBilgi.kayittarihi + "\n" +
+                            "Cinsiyeti = " + kisiselBilgi.cinsiyeti );
+        }
+    }
+}
